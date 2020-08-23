2098a1da290d1709d71a1c00663f1456b5445cca
Iunaldi <ilkerunaldi76@gmail.com>
2020-08-23 18:47:35 -0400

    master update1

--- src/Cay/Cay1.java
+++ src/Cay/Cay1.java
@@ -5,8 +5,8 @@ public class Cay1 {
 
     public static void main(String[] args) {
 
-        System.out.println("deneme1");
-        System.out.println("deneme1");
+        System.out.println("deneme12");
+        System.out.println("deneme12");
 
     }
 }
